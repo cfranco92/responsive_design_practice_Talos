3b65991d6fd44d27f88ded1f9fafc0a330efee18
Cristian Franco Bedoya <cfrancobedoya@gmail.com>
2021-01-28 13:24:26 -0500

    Adding main js for fix spy navbar.

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbarTogglerDemo01'
+})
+
+var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
+dataSpyList.forEach(function (dataSpyEl) {
+  bootstrap.ScrollSpy.getInstance(dataSpyEl)
+    .refresh()
+})
+
+// var scrollSpyContentEl = document.getElementById('content')
+// var scrollSpy = bootstrap.ScrollSpy.getInstance(scrollSpyContentEl) // Returns a Bootstrap scrollspy instance
